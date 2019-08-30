a9e78cb3577c54529bd53147f2846fcea66a3c9d
Chloé Dequeker <noreply@nelyah.eu>
2019-08-30 17:29:28 +0200

    command-handler: Add /standings command handler

--- src/main.py
+++ src/main.py
@@ -49,23 +49,23 @@ def startup_browser():
 
     return browser
 
-def get_web_data():
+
+def get_regular_season_standings(browser_driver):
     url = 'https://eu.lolesports.com/en/league/lec'
     url = 'https://watch.lolesports.com/vods/lec/lec_2019_summer'
     url = 'https://watch.lolesports.com/standings'
     url = 'https://watch.lolesports.com/standings/lec/lec_2019_summer/regular_season'
 
-    driver = startup_browser()
+    browser_driver = startup_browser()
 
     logging.log(logging.INFO, f'Getting url: {url}')
-    driver.get(url)
+    browser_driver.get(url)
 
     # Poll the website because selenium can't wait implicitly
     while True:
-        page_source = driver.page_source
+        page_source = browser_driver.page_source
         soup = BeautifulSoup(page_source, 'lxml')
         t_team_data = soup.findAll(class_='ranking')
-        print(t_team_data)
 
         if len(t_team_data) > 0:
             break
@@ -87,9 +87,8 @@ def get_web_data():
 
         t_team_dict.append(d_team)
 
-    print(t_team_dict)
+    return t_team_dict
             
-    driver.quit()
 
 
 def get_url():
@@ -115,21 +114,52 @@ def audio(bot, context):
     bot.send_message(chat_id=context.message.chat_id, text="No use sending me audio...")
 
 
-def main():
+def bot_get_rankings(bot, context):
+    """Scrape website for ranking data and print a nicely formatted output.
+
+    """
+    # Connect to existing browser
+    browser_driver = startup_browser()
+    t_team_data = get_regular_season_standings(browser_driver)
+
+    msg = ''
+    for team_data in t_team_data:
+        msg += f"{team_data['rank']} - {team_data['name']}: {team_data['win']}W-{team_data['loss']}L\n"
+
+    bot.send_message(chat_id=context.message.chat_id, text=msg)
+
+
+def startup_bot():
+    """Startup function for the Telegram Bot
+    :returns: Updater object
+
+    """
+
     config = configparser.ConfigParser()
     config.read(path.join(path.dirname(path.abspath(__file__)), 'config.py'))
     api_key = config.get('API', 'key')
 
     updater = Updater(api_key)
+    return updater
+
+
+def main():
+    browser_driver = startup_browser()
+
+    updater = startup_bot()
+
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('standings', bot_get_rankings))
     dp.add_handler(MessageHandler(Filters.text | Filters.photo, echo))
 
-
     updater.start_polling()
+    logging.log(logging.INFO, 'Listening for input...')
+
     updater.idle()
+
+    browser_driver.quit()
     
 
 if __name__ == '__main__':
-    # main()
-    get_web_data()
+    main()
